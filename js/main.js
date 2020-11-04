146c39a1cf6bec7ecf36f7aefeb3395ec819a637
Gianmarco <gianmarco.cavalieredev@gmail.com>
2020-11-04 16:45:07 +0100

    Esercizio Completo!

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.has-dropdown').click(function(){
+        $('.has-dropdown').children().removeClass('active');
+        $(this).children().addClass('active');
+    })
+});
